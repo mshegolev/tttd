2324e9bd3c663e6a43666edd8d46bf7e23ab5eb5
admin <admin@iMac-admin.local>
2019-03-22 00:51:52 +0300

    some fix v10

--- plansite/news/urls.py
+++ plansite/news/urls.py
@@ -4,6 +4,7 @@ from news.models import Articles
 
 urlpatterns = [
     path('', ListView.as_view(queryset=Articles.objects.all().order_by("-date")[:20],
-    template_name = "news/posts.html"), name='index'),
+    template_name = "news/posts.html")),
+    path('<int:pk>',DetailView.as_view(model=Articles,template_name='news/post.html')),
 
 ]
