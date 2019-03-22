35a978c76af9cc428e0cd361745393894c74fb74
Щеголев Михаил Владимирович <Mikhail.Shchegolev@rtits.ru>
2019-03-22 18:53:03 +0300

    add goal page

--- plansite/goal/models.py
+++ plansite/goal/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Goals(models.Model):
+    title = models.CharField(max_length=120)
+    post = models.TextField()
+    cost = models.IntegerField()
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
--- plansite/goal/urls.py
+++ plansite/goal/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from . import views
+from django.views.generic import ListView, DetailView
 
-urlpatterns = [
-    path('', views.index, name='index'),
+from goal.models import Goals
+
+urlpatterns = [  # path('', views.index, name='index'),
+    path('', ListView.as_view(queryset=Goals.objects.all().order_by("-date")[:20], template_name="goal/goals.html")),
+    path('<int:pk>', DetailView.as_view(model=Goals, template_name='goal/goal.html')),
+
 ]
