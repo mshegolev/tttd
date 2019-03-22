2d6b61beb51813cd6d85dffa6e7534efd776a137
Щеголев Михаил Владимирович <Mikhail.Shchegolev@rtits.ru>
2019-03-22 19:39:42 +0300

    add goal page

--- plansite/goal/views.py
+++ plansite/goal/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h3> Hello world! </h3>")
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Goals
+
+class GoalsList(ListView):
+    model = Goals
